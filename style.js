9f558e744a70ee57cdf211bd9d2991b6ae6950e3
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2021-11-21 21:01:12 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,113 @@
+function read_more_less()
+{
+    var dots =document.getElementById('dots');
+    var moretext =document.getElementById('more');
+    var mybtn =document.getElementById('mybtn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
+
+function read_moree_lesss()
+{
+    var dots =document.getElementById('dotss');
+    var moretext =document.getElementById('moree');
+    var mybtn =document.getElementById('mybtnn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
+
+function read_moree_lessss()
+{
+    var dots =document.getElementById('dotsss');
+    var moretext =document.getElementById('moreee');
+    var mybtn =document.getElementById('mybtnnn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
+
+function read_moree_lesssss()
+{
+    var dots =document.getElementById('dotssss');
+    var moretext =document.getElementById('moreeee');
+    var mybtn =document.getElementById('mybtnnnn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
+
+function read_moree_lessssss()
+{
+    var dots =document.getElementById('dotsssss');
+    var moretext =document.getElementById('moreeeee');
+    var mybtn =document.getElementById('mybtnnnnn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
+
+function read_moree_lesssssss()
+{
+    var dots =document.getElementById('dotssssss');
+    var moretext =document.getElementById('moreeeeee');
+    var mybtn =document.getElementById('mybtnnnnnn');
+
+    if(dots.style.display ==='none')
+    {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML='Read more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML='Read less';
+    }
+}
