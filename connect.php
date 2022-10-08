0aab2bec886829dc3ea931c1790c73641e735100
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2022-10-08 19:34:56 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
+	$message = $_POST['message'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		// echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, gender, email,number, message) values(?, ?, ?, ?,?)");
+		$stmt->bind_param("sssis", $firstName, $gender, $email,$number, $message);
+	    $stmt->execute();
+		// echo $execval;
+		echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
+		$stmt->close();
+		$conn->close();
+	}
+?>
